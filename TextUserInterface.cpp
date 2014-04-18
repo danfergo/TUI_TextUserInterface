c591d7dc7f5b1bd0a6e0006139f7e941cb8733cd
danfergo <danfergo@gmail.com>
2014-04-18 14:57:19 +0100

    Hello World commit.

---
+++ TextUserInterface.cpp
@@ -0,0 +1,9 @@
+/*
+ * TextUserInterface.cpp
+ *
+ *  Created on: 18/04/2014
+ *      Author: Daniel
+ */
+
+#include "TextUserInterface.h"
+
---
+++ TextUserInterface.h
@@ -0,0 +1,14 @@
+/*
+ * TextUserInterface.h
+ *
+ *  Created on: 18/04/2014
+ *      Author: Daniel
+ */
+
+#ifndef TEXTUSERINTERFACE_H_
+#define TEXTUSERINTERFACE_H_
+
+class TextUserInterface {
+};
+
+#endif /* TEXTUSERINTERFACE_H_ */
